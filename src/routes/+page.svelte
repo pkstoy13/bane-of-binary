b77a16497340c7a688c94ecefaf29a87e445ff71
pkstoy13 <pkstoyanov1@gmail.com>
2024-07-12 15:19:35 -0400

    installed tailwind and skeleton, figuring out how skeleton works, and made custom tab icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="flex h-screen items-center justify-center text-4xl">b4n3_0f_b1n4ry_</h1>
